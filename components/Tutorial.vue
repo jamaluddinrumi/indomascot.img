ba5b8fd56d13a2c90447970b4f15f76ae5317648
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-07-29 16:23:49 +0000

    fix: delete bloated class styles

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,13 +1,13 @@
 <!-- Please remove this file from your project -->
 <template>
   <div
-    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+    class="min-h-screen bg-gray-100"
   >
     <link
       href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css"
       rel="stylesheet"
     />
-    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8 flex items-center justify-center">
       <a
         class="flex justify-center pt-8 sm:pt-0"
         href="https://www.indomascot.com"
