27f761f0d04c44421f3f9e1a8eb4bb50e07363f3
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-07-29 16:38:04 +0000

    fix: load tailwind css link tag globally

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,13 +15,13 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css' }
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
